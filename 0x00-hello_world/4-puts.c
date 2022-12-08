8b6021c6f05e09bb1f7c1da9eca0f805c9bfdc7c
Chey04 <vchukwukere123@gmail.com>
2022-12-08 05:48:12 -0800

    yet another

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * Program to print out string
-*/
+ * main - prints the phrase Programming is like building a multilingual puzzle
+ * return - 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
