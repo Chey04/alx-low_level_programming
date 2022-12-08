df780d8ebc1db00c9b5567fe9811bc740d1726c3
Chey04 <vchukwukere123@gmail.com>
2022-12-08 06:04:13 -0800

    forgot void arg in main

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-int main() 
+/** main - using printf function to output with proper grammer, but the outcome is a piece of art,
+ */
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf ("with proper grammer, but the outcome is a piece of art,\n");
 
 	return(0);
 }
