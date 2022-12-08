206ba6b877a77872d7f1a9abbab87c207010f4a4
Chey04 <vchukwukere123@gmail.com>
2022-12-08 05:54:06 -0800

    another try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints the phrase Programming is like building a multilingual puzzle
- * Return - 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
-	char newString[] = "\"Programming is like building a multilingual puzzle\n";
+	char newString[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(newString);
 	return (0);
