13738fb6911fbafd22c5327e25004fdedbe5602c
Chey04 <vchukwukere123@gmail.com>
2022-12-08 12:10:42 -0800

    stdin=0 stdout =1 stderr=2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - prints out required string
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+
+}	
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,5 @@
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-
 	return (0);
 }
