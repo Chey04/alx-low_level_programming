726699a87e6c6ec841f1fdfe9207e4f2fb82a075
Chey04 <vchukwukere123@gmail.com>
2022-12-08 05:51:05 -0800

    needed an error apperently

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints the phrase Programming is like building a multilingual puzzle
- * return - 0 if exited properly, non-zero otherwise
+ * Return - 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
-	char newString[] = "Programming is like building a multilingual puzzle\n";
+	char newString[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(newString);
 	return (0);
