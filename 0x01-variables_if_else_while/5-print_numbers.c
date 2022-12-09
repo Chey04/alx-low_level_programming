0830210cd0d0cfa75bac92e22e18e5c980c3b957
Chey04 <vchukwukere123@gmail.com>
2022-12-09 09:00:03 -0800

    first push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - list out 0 - 9
+ * Return: 0 if sucessful, non-zero otherwise
+ */
+
+int main(void)
+
+{
+	for (n = 0; n > 10; n++)
+		printf("%d", n);
+	printf('\n');
+
+
+
+	return (0);
+}
