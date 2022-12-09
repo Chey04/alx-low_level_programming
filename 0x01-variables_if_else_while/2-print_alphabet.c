465b0dc8242b6d407296baa96045f51601d1fae3
Chey04 <vchukwukere123@gmail.com>
2022-12-09 08:02:29 -0800

    first push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - change from uppercase letters to lowercase letters
+ * without using printf fucnction
+ * Return: must equal 0 if comeplete otherwise print non-zero
+ */
+
+int main(void)
+
+{
+
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		putchar(alpha);
+
+
+	return (0);
+}
