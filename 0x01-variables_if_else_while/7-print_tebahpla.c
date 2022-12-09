12de2fc7450c78e96c82eb8a99ff5b6015b27620
Chey04 <vchukwukere123@gmail.com>
2022-12-09 09:17:57 -0800

    added files

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print letters backwards
+ * Return: 0 for success
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (char = 'z'; char >= 'a'; char--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+
+
+
+
+	return (0);
+}
