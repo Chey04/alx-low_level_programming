42ba52ca3816247d41892db0de239aa65fb79b2d
Chey04 <vchukwukere123@gmail.com>
2022-12-09 09:09:47 -0800

    commitiing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print out list of numbers
+ * Return: 0 if sucessful
+ */
+
+int main(void)
+
+
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n')
+
+
+
+
+	return (0);
+}
