72f7bb653b09de37e64c2b7a6823a13136234de6
Chey04 <vchukwukere123@gmail.com>
2022-12-13 10:04:19 -0800

    edit files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,14 @@
 /**
  * _islower - checks if a input
  * is a lowercase alphabet
- *
+ *@c: is the characetr to check
  * Return: 0 if false and 1 if correct
  */
 
 
 
-int _islower(int)
+int _islower(int c)
 {
-	int c;
 
 	if ((c <= 'a') || (c >= 'z'))
 		return (0);
