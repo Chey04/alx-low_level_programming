018c439f459ccfee5ea782a1a0cb86e49459616e
Chey04 <vchukwukere123@gmail.com>
2022-12-13 10:34:33 -0800

    edited files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * print_sign - prints out the sign of a
  * given number
  *
@@ -9,13 +9,17 @@
  * Return: 0 if sucessful, non zero otherwise
  */
 
-int print_sign (int n)
+int print_sign(int n)
 {
 	if (n > 0)
+		_putchar('+');
 		return (1);
+
 	else if (n > 0)
+		_putchar('-');
 		return (-1);
-	else 
+	else
+		_putchar(0);
 		return (0);
 
 
