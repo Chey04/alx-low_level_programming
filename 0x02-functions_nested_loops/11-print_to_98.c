6a02569802527aeb1ec25e367a75b3a712b8d7c2
Chey04 <vchukwukere123@gmail.com>
2022-12-13 13:10:50 -0800

    added files

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,30 @@
 
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		printf("%d, ", i);
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+
 	}
-
-
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j ==98)
+				printf("%d\n", j);
+		}
+	
+	}
 
 
 
