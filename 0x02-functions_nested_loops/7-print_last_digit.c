c67496e60f543fd88777c8e00329b1c5378807b6
Chey04 <vchukwukere123@gmail.com>
2022-12-13 11:37:23 -0800

    added files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints out the last digit
+ * of a given variablee
+ *
+ * @n: interger to be checked
+ *
+ * Return: always 0
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+
+
+
+
+
+}
