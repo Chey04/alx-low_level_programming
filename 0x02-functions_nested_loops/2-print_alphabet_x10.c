2fc41c5d9f1efda9fbdcedc24c1d58e0e3e4ffbb
Chey04 <vchukwukere123@gmail.com>
2022-12-13 03:55:41 -0800

    Added files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/** print_alphabet_x10 - prints out
+ * lowercase alphebets 10 times
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = i; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	_putchar('\n');
+	}
+
+
+}
