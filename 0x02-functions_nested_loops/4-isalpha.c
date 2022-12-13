a33a5aa60c2bbe12903600b95f7845d1973def4e
Chey04 <vchukwukere123@gmail.com>
2022-12-13 10:14:11 -0800

    added files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints out letters
+ *
+ * @c: character to check
+ *
+ * Return: 1 if c is alphabet, 0 otherwise
+ */
+
+
+int _isalpha(int c)
+{
+	if ((c <= 'a' && c>= 'z') || (c <= 'A' && c >= 'Z'))
+		return (1);
+	else
+		return (0);
+
+
+
+
+
+}
