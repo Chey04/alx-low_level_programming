9ff33f5e9798d393b41e71f69393525bba010eca
Chey04 <vchukwukere123@gmail.com>
2022-12-13 10:25:43 -0800

    added files

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** 
+ * print_sign - prints out the sign of a
+ * given number
+ *
+ * @n: variable to be checked
+ *
+ * Return: 0 if sucessful, non zero otherwise
+ */
+
+int print_sign (int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n > 0)
+		return (-1);
+	else 
+		return (0);
+
+
+
+
+}
