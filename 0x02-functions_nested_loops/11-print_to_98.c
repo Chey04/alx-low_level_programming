433ec26a5430ea5965a00ae254f4302c18d2247b
Chey04 <vchukwukere123@gmail.com>
2022-12-13 12:54:12 -0800

    added files

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers
+ * from n to 98
+ *
+ * @n: starting number
+ *
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+		_putchar(i);
+		_putchar(',');
+		_putchar(' ');	
+
+
+
+
+
+
+
+
+
+}
