424a77c48421b5c12fcafa4745d45b82c737a02f
Chey04 <vchukwukere123@gmail.com>
2022-12-13 12:46:12 -0800

    added files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * add - adds up two integers
+ *
+ * @i: first int
+ *
+ * @j: second int
+ *
+ * Return: always 0
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+
+	return (k);
+
+
+
+
+
+
+
+
+
+}
