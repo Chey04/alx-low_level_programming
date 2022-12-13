fad82a69e32feea150892ea53b47d7d381016ea2
Chey04 <vchukwukere123@gmail.com>
2022-12-13 10:37:41 -0800

    edited files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,17 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar('+');
 		return (1);
+		_putchar('+');
+
 
 	else if (n > 0)
-		_putchar('-');
 		return (-1);
+		_putchar('-');
+
 	else
-		_putchar(0);
 		return (0);
+		_putchar(0);
 
 
 
