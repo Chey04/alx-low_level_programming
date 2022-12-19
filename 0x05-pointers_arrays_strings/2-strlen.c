33c29c99392dacb042de380691f0a26f7e57808a
Chey04 <vchukwukere123@gmail.com>
2022-12-19 05:15:09 -0800

    added files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string address
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s[i] != '\0'; i++)
+	{
+		return(i);
+	}
+}
