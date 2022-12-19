8ee0496189fd0022ef703c58545c81e9d1cfb863
Chey04 <vchukwukere123@gmail.com>
2022-12-19 06:56:35 -0800

    added files

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in a string
+ * @str: string input
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i + 2)
+		_putchar (str[i]);
+	_putchar('\n');
+}
