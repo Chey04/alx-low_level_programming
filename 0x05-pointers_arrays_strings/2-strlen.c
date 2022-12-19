f44ffccc7f56df863e21b1f05dafae57a89fa5d2
Chey04 <vchukwukere123@gmail.com>
2022-12-19 05:33:03 -0800

    edited files

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,14 @@
 /**
  * _strlen - returns the lenght of a string
  * @s: string address
+ * Return: 0
  */
 
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 
 
 	return (i);
