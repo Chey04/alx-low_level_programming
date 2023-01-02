b48aae8dd03f767518477f53a12d5e251c85f78b
Chey04 <vchukwukere123@gmail.com>
2023-01-02 04:38:45 -0800

    added files

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a pointer to char
+ *
+ * @s: content is to be copied
+ *
+ * @to: string
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
