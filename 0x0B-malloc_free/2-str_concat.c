3ebe2034a86609fdf47803ea6cfcc2bbcf9a645d
Chey04 <vchukwukere123@gmail.com>
2023-01-09 05:19:10 -0800

    fixed bugs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, len;
-	char *ptr, str;
+	char *ptr, str[];
 
 
 
@@ -21,11 +21,14 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	for (len = 0; s1[len]; len++)
-		;
+		str[len] = s1[len];
+
+	len++;
 
 	for (i = 0; s2[i]; i++)
 	{
-		str = s1[len] + s2[i];
+		str[len] = s2[i];
+		len++;
 	}
 
 	ptr = (char *) malloc(sizeof(char) * (i+len));
