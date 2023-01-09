f13af3dd01b5ee0588483d0a2a3731acc9f60265
Chey04 <vchukwukere123@gmail.com>
2023-01-09 03:13:24 -0800

    edited files

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,16 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
-	int i;
+	unsigned int i;
 
 	if (size == '\0')
 		return (NULL);
 
 	ptr = (char *) malloc(size * sizeof(char));
 
-	for (i = 0; i <= size i++)
+	for (i = 0; i <= size; i++)
 	{
-		*ptr[i] = c;
+		ptr[i] = c;
 	}
 
 	return (ptr);
