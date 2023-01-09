5770351fc4a069738d98b0f88f8187943126eeca
Chey04 <vchukwukere123@gmail.com>
2023-01-09 10:29:08 -0800

    added files

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,10 +11,10 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height == NULL)
+	if (grid == NULL || height == 0)
 		return;
 
-	for (i < 0; i < height; i++)
+	for (i = 0; i < height; i++)
 		free(grid[i]);
 
 	free(grid);
