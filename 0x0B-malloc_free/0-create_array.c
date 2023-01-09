d99f69860fe4e7e7647f91e446b65a944f4267db
Chey04 <vchukwukere123@gmail.com>
2023-01-09 10:27:15 -0800

    added files

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,9 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *) malloc(size * sizeof(char));
 
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i <= size; i++)
 	{
 		ptr[i] = c;
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - function to free grid memory
+ * @grid: location of memory to free
+ * @height: height of grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == NULL)
+		return;
+
+	for (i < 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
