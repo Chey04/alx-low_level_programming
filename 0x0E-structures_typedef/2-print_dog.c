72afa15ce1fa4a9142daa05a82ad5936202c5f12
Chey04 <vchukwukere123@gmail.com>
2023-01-16 04:18:40 -0800

    added files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)");
 	printf("Name: %s\n", (*d).name);
 
-	if ((*d).age == 0)
+	if ((*d).age == 0.0)
 		printf("Age: (nil)");
 	printf("Age: %f\n", (*d).age);
 
