ecaa468e8fb963b12ce2191250879f7388245af6
Chey04 <vchukwukere123@gmail.com>
2023-01-18 14:46:02 -0800

    fixed bugs

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
-#include "function_pointers"
+#include "function_pointers.h"
 
 /**
  * print_name: function to print a name
  * @name: name to be printed
  * @f: callback function
+ * Return: void
  */
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }
