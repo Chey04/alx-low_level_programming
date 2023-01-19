35a3a98d548c084d022dbcafc2887cc6c0ebb9dd
Chey04 <vchukwukere123@gmail.com>
2023-01-18 18:01:21 -0800

    fixed up betty

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,8 +16,8 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-   	 };
-	
+	};
+
 	int i = 0;
 
 	while (i < 5)
@@ -26,8 +26,8 @@ int (*get_op_func(char *s))(int, int)
 			return (ops[i].f);
 
 		i++;
-       	}
-	
+	}
+
 	return (NULL);
 
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
 
 int op_add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
 
 /**
@@ -19,7 +19,7 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-        return(a - b);
+	return (a - b);
 }
 
 
@@ -32,7 +32,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-        return(a * b);
+	return (a * b);
 }
 
 
@@ -45,7 +45,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-        return(a / b);
+	return (a / b);
 }
 
 
@@ -59,6 +59,6 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-        return(a % b);
+	return (a % b);
 }
 
