4d8a5d231d38df451b144f1009a66c3f1480bdbe
Chey04 <vchukwukere123@gmail.com>
2023-01-18 17:14:56 -0800

    added new file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,4 +7,11 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
+
+
 #endif
