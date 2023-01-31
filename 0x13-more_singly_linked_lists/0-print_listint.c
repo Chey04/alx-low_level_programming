a6751f9595337f7510805ca34fd81d67727efed1
Chey04 <vchukwukere123@gmail.com>
2023-01-31 08:34:32 -0800

    fixed more bugs

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,7 +15,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h->next != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,7 +12,6 @@ size_t listint_len(const listint_t *h)
 
 	if (h == NULL)
 		return (0);
-	
 
 	while (h != NULL)
 	{
