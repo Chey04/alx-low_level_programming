670f06b6f178a7fd13dcc5b8f06f1a9a047c320c
Chey04 <vchukwukere123@gmail.com>
2023-01-31 03:52:12 -0800

    corrected dir name

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint: deletes head of node
+ * @head: pointer to first node
+ * Return: element of first node
+ */
+
+int pop_listint(listint_t **head)
+{
+	int first_node;
+	listint_t *temp, *next;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	next = temp->next;
+	first_node = temp->n;
+
+	free(temp);
+
+	*head = next;
+
+	return (first_node);
+}
