358ab35e595f3d4487182c29d50c1ceb4da915f1
Chey04 <vchukwukere123@gmail.com>
2023-02-06 04:05:42 -0800

    added files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef IO_
+#define IO_
+
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+int _strlen(const char *s);
+
+#endif
