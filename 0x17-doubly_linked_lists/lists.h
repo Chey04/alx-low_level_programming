ff6c21ee284ca719eca8b65f9648ff1c2e449922
Chey04 <vchukwukere123@gmail.com>
2023-03-16 05:22:05 -0700

    Added files

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,4 @@
-#ifndef LISTS_H
-#define LISTS_H
+
 
 /**
  * struct dlistint_s - doubly linked list
@@ -28,4 +27,3 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 
-#endif
